9cb06a261010d799b530d648f9131900f06c0b7b
lamoanh13420 <lamoanh13420@gmail.com>
2021-06-20 09:09:25 +0700

    commit5

---
+++ DoAn/DoAn/Areas/Admin/Controllers/DatVeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DoAn.Models;
+
+namespace DoAn.Areas.Admin.Controllers
+{
+    public class DatVeController : Controller
+    {
+        DulichEntities db = new DulichEntities();
+        // GET: Admin/LichTrinhs
+        public ActionResult Index()
+        {
+            return View(db.DatVes.ToList());
+        }
+    }
+}
